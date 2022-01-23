46f311ce127f5d6be611e4b8983369bef9d9d178
Franco <francodlopez98@gmail.com>
2022-01-23 13:01:10 -0300

    primer desafio

--- main.js
+++ main.js
@@ -1,62 +1,29 @@
-//hola 
 
-/* let numero = prompt("Ingresa un numero");
-
-if (numero == 5) {
-
-    console.log("Hola " + numero);
-
-} else{
-    console.log("Tu no eres " + numero);    
-}
-*/
-
-/*let texto = "Nombre";
-let numero = 123;
-let boolean = 5 > 10;
-
-console.log(typeof(texto));
-console.log(typeof(numero));
-console.log(typeof(boolean));
-*/
-
-/* 
-Propuesta de ejercicio
-- Según la edad del usuario imprimir los siguientes mensajes:
-  · Menor o igual a 18 años: tienes toda una vida por delante. 
-  · Mayor de 18 y menor o igual a 35: que grande que estás.
-  · Mayor a 35 y menor o igual a 65: estás muy grande para andar con jueguitos.
-  · En cualquier otro caso: que bueno que puedas dedicar tiempo a jugar.
-*/
+let nombre = prompt ("Hola, Indicanos tu nombre");
 
 
-/*const YEAR = 2022;
-let nombre = prompt(`Nombre`);
-let anioNacimiento = prompt(`Año de nacimiento`);
-let edad = YEAR - anioNacimiento;
-
-let salida = `Bienvenido ${nombre} tienes ${edad} años.`;
-
-if (edad <= 18) {
-    alert(salida + " Tienes toda una vida por delante.");
-}else if ((edad > 18) && (edad <= 35)) {
-    alert(salida + " Que grande que estás.");
-}else if ((edad > 35) && (edad <= 65)){
-    alert(salida + " Estás muy grande para andar con jueguitos.");
-}else 
-    alert(salida + " Que bueno que puedas dedicar tiempo a jugar.");
-    */
-
-    /*for (let i = 0; i < 10; i++) {
-         alert(i)
+while (nombre === ""){
+    nombre = prompt("Por favor ingresa tu nombre");
     }
-*/
 
-let entrada = prompt("Ingresa un numero: ")
+alert("Bienvenido " + nombre + " tendras que adivinar un numero del 1 al 10, si lo haces ganas");
+alert("Tan solo cuentas con 3 intentos, piensa bien antes de elegir");
+let num = parseInt (prompt ("Elije un numero del 1 al 10, a ver si hay suerte"));
+const num1 = Math.round(Math.random(1,10) * (10-1) + 1);
+console.log(num1)
 
-while (entrada < 5) {
-    alert(entrada);
-    entrada = prompt("Ingresa otro numero: ");
-}
+for (let i = 1; i <= 3; i++){
+        let num2 = 3 - i
+        if (num === num1){
+            alert("Felicidades " + nombre + " adivinaste en tu intento numero " + i + " , gracias por jugar");
+            break
+        } else if (i !== 3){
+            alert("Que lastima, esta vez no acertaste. Te quedan " + num2 + " intentos" );
+            num = parseInt (prompt ("Prueba con otro numero"));
+        } else {
+            alert("Que lastima, esta vez no acertaste. Te quedan " + num2 + " intentos" );
+            alert("Espero tengas mas suerte la proxima");
+        }
+    }
 
-// hola
+        
