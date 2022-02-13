f03a14fc505c134f355e27bef01e4617543f9e9b
Franco <francodlopez98@gmail.com>
2022-02-13 16:51:02 -0300

    DOM

--- baseDeDatos.js
+++ baseDeDatos.js
@@ -1,25 +1,10 @@
-let numMenu = 0
-let numProduct = 0
-let cantProduct = 0
-let pago = ""
-let nombre = ""
-let dni = 0
-let numTarj = 0
-let numPin = 0
-let total = 0
-let cuota = 0
-let carrito = []
-let sumapre = 0
-let carrito1 = []
-let arrayAlert = []
-let buscar = ""
-let filtrar = ""
-let juegos = ""
 
-
-const productos = [{id: 1, nombre: "Grand Theft Auto V", cantidad: 15, precio: 1000, categoria: "accion"},
-{id: 2, nombre: "Assassin's Creed Valhalla", cantidad: 7, precio: 3000, categoria: "accion"},
-{id: 3, nombre: "Red Dead Redemption 2", cantidad: 8, precio: 1500, categoria: "aventura"},
-{id: 4, nombre: "It Take Two", cantidad: 10, precio: 2000, categoria: "multijugador"},
-{id: 5, nombre: "Cyberpunk 2077", cantidad: 6, precio: 2500, categoria: "rol"},
+const productos = [{id: 1, nombre: "Grand Theft Auto V", plataforma: "PS4/PC/XBOX", precio: 1000, categoria: "Accion", imagen: "./imag/gta5.webp"},
+{id: 2, nombre: "Assassin's Creed Valhalla", plataforma: "PS4/PC/XBOX", precio: 3000, categoria: "Accion", imagen: "./imag/valhalla.webp"},
+{id: 3, nombre: "Red Dead Redemption 2", plataforma: "PS4/PC/XBOX", precio: 1500, categoria: "Aventura", imagen: "./imag/red2.webp"},
+{id: 4, nombre: "It Take Two", plataforma: "PS4/PC/XBOX", precio: 2000, categoria: "Multijugador", imagen: "./imag/it.webp"},
+{id: 5, nombre: "Cyberpunk 2077", plataforma: "PS4/PC/XBOX", precio: 2500, categoria: "Rol", imagen: "./imag/cyber.webp"},
+{id: 6, nombre: "Dying Light 2 Stay Human", plataforma: "PS4/PC/XBOX", precio: 3500, categoria: "Accion", imagen: "./imag/dying2.webp"},
+{id: 7, nombre: "Gran Turismo 7", plataforma: "PS4", precio: 4000, categoria: "Carreras", imagen: "./imag/gran7.webp"},
+{id: 8, nombre: "Battlefield 2042", plataforma: "PS4/PC/XBOX", precio: 4500, categoria: "Accion", imagen: "./imag/battlefield2042.webp"},
 ]
--- funciones.js
+++ funciones.js
@@ -1,59 +1,29 @@
 
-const validar = (numMin, num, numMax, msj) => {
-    while ((numMin > num) || (num > numMax) || (isNaN (num) == true ) || (num == " ")){
-        num = parseInt(prompt (msj))    
-    }
-    return parseInt(num)
-}
+let lista = document.createElement("div");
+let lista1 = document.getElementById("Productos")
 
-const validarTarj = (numMin, num, numMax, msj) => {
-    while ((numMin > num.toString().length) || (num.toString().length > numMax) || (isNaN (num) == true ) || (num == " ")){
-        num = parseInt(prompt (msj))    
-    }
-    return parseInt(num)
-}
 
-const pagos = () => {
-    nombre = prompt("Ingresa tu nombre completo");
-    while ((nombre == " ") || (isNaN (nombre) == false)){
-        nombre = prompt("Ingresa tu nombre nuevamente porfavor")
-    }
-    dni = parseInt(prompt("Ingresa tu DNI"));
-    validarTarj (7,dni,8,"El DNI no es correcto. Ingresalo nuevamente")
-    numTarj = parseInt(prompt("Ingresa el numero de tarjeta"));
-    validarTarj (16,numTarj,16,"El numero de tarjeta es incorrecto. Ingresalo nuevamente")
-    numPin = parseInt(prompt("Ingresa el CVC"));
-    validarTarj (3,numPin,3,"El CVC no es correcto. Porfavor ingresalo nuevamente")
-    return (nombre, dni, numTarj, numPin)
-}
 
-const cuotas = (cantCuotas, pre) => {
-    while ((cantCuotas !== 3)  && (cantCuotas !== 6) && (cantCuotas !== 12)) {
-        alert ("El numero de cuotas ingresado no es el correcto")
-        cantCuotas = parseInt(prompt("Puedes elegir pagar en 3 con interes del 10%, 6 con interes del 30% o 12 cuotas con interes del 60%"))
-    }
-    if (cantCuotas === 3){
-        total = pre * 1.1
-    } else if (cantCuotas === 6) {
-        total = pre * 1.3
-    } else if (cantCuotas === 12){
-        total = pre * 1.6
-}
-    return (Math.round(total))
-}
-
-const cantTotal = () => {
-    for (const product of carrito1){
-        sumapre += product.precio
-    } 
-    return sumapre
-
-}
-
-const mostrar = (array1, array2) => {
+const mostrar = (array1) => {
     let indice = 0;
-    for (const product of array2){
-        indice += 1; 
-        array1.push(`${indice} - Nombre: ${product.nombre} - Cantidad: ${product.cantidad} - Precio: ${product.precio}\n`)
+    for (const product of array1){
+        lista.innerHTML += `
+      <div class="card" style="width: 18rem;">
+        <div class="imagen">
+            <img src= ${product.imagen} class="card-img-top" alt="...">
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">${product.nombre}</h5>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Precio: $${product.precio}</li>
+          <li class="list-group-item">Plataforma: ${product.plataforma}</li>
+          <li class="list-group-item">Categoria: ${product.categoria}</li>
+          <a href="#" class="btn btn-primary">Añadir al Carrito</a>
+        </ul>
+    </div>`;
     }
+    lista1.append(lista);
+    lista.className = "productStyle";
 }
+
