215171f51399ef5d4a7ef70ab23f47917cd25173
Franco <francodlopez98@gmail.com>
2022-02-17 20:33:23 -0300

    Eventos

--- funciones.js
+++ funciones.js
@@ -1,11 +1,7 @@
-
 let lista = document.createElement("div");
-let lista1 = document.getElementById("Productos")
-
-
+let lista1 = document.getElementById("Productos");
 
 const mostrar = (array1) => {
-    let indice = 0;
     for (const product of array1){
         lista.innerHTML += `
       <div class="card" style="width: 18rem;">
@@ -17,13 +13,32 @@ const mostrar = (array1) => {
         </div>
         <ul class="list-group list-group-flush">
           <li class="list-group-item">Precio: $${product.precio}</li>
-          <li class="list-group-item">Plataforma: ${product.plataforma}</li>
+          <li class="list-group-item">Plataforma: ${product.plataforma.join("/")}</li>
           <li class="list-group-item">Categoria: ${product.categoria}</li>
-          <a href="#" class="btn btn-primary">Añadir al Carrito</a>
+          <button id= "boton${product.id}" href="#" class="btn btn-primary">Añadir al Carrito</button>
         </ul>
     </div>`;
     }
     lista1.append(lista);
-    lista.className = "productStyle";
+    lista.id = "productStyle";
 }
+
+
+
+let error1 = document.getElementById("cuerpo");
+
+
+const error = (imag) => {
+  let divContainer = document.createElement("div")
+  divContainer.style = "display: flex; justify-content: center; align-items: center; flex-direction: column; align-content: center; margin-top: 500px;"
+  let img = document.createElement("img")
+  img.src = imag
+  let titulo = document.createElement("h1")
+  titulo.innerHTML = "PRODUCTO NO ENCONTRADO"
+  divContainer.append(img)
+  divContainer.append(titulo)
+  error1.append(divContainer)
+  
+}
+
 
--- main.js
+++ main.js
@@ -1,5 +1,96 @@
 
 mostrar(productos);
 
+//Filtra por categoria
 
+let categoria = document.getElementsByClassName("dropdown-item");
+for (cat of categoria) {
+  cat.addEventListener("click", e => { 
+  $("#productStyle").empty();
+  const selected = productos.filter(product => product.categoria == e.target.id);
+  mostrar(selected);
+  })
+}
+
+//Filtra por plataforma
+
+let plataforma = document.getElementsByClassName("dropdown-item 1");
+for (plat of plataforma) {
+  plat.addEventListener("click", e => { 
+  $("#productStyle").empty();
+  const recorrer = productos.map((el) => {
+    for (let l of el.plataforma){
+      if (l == e.target.id){
+        nuevoArray.push(el);
+      } 
+     }
+   }
+  )
+mostrar(nuevoArray);
+nuevoArray.splice(0,nuevoArray.length);
+  })
+}
+
+// Añadir al carrito
+
+let carro = document.getElementsByClassName("btn btn-primary");
+for (cart of carro) {
+  cart.addEventListener("click", e => {
+  e.target.innerText = "Producto Añadido";
+  e.target.disabled = true;
+  const selected = productos.find(product => `boton${product.id}` == e.target.id);
+  nuevoArray2.push(selected);
+  let contador = document.getElementById("circulo");
+  let cont = document.createElement("b");
+  cont.innerHTML = nuevoArray2.length;
+  $("#circulo").empty();
+  contador.appendChild(cont);
+  })
+}
+
+// Mostrar Carrito
+
+let mostCarro = document.getElementById("car");
+  mostCarro.addEventListener("click", () => { 
+  if (nuevoArray2.length > 0){
+  $("#productStyle").empty();
+  mostrar(nuevoArray2);
+  let carro1 = document.getElementsByClassName("btn btn-primary");
+  for (car1 of carro1) {
+  car1.innerText = "Quitar del Carrito"
+  } 
+  }
   
+  else{
+    $("#productStyle").empty();
+    $("#cuerpo").empty();
+    error("./imag/remove.png")
+    
+  }
+})
+
+
+//Barra de busqueda
+
+let teclado = document.getElementById("buscador");
+let buscar = document.getElementById("buscador2");
+buscar.addEventListener("click", () => {
+  const selected1 = productos.filter(item => {
+    if (item.nombre.toLowerCase().includes(teclado.value.toLowerCase())){
+      return item
+    }
+  });
+  if (selected1.length > 0){
+    $("#productStyle").empty();
+    $("#cuerpo").empty();
+    console.log(selected1)
+    mostrar(selected1)
+  }
+  else{
+    $("#productStyle").empty();
+    $("#cuerpo").empty();
+    error("./imag/remove.png")
+    
+  }
+  
+})
