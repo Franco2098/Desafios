7ea1118f7897663ca9a7f45241b2484e35fe25c9
Franco <francodlopez98@gmail.com>
2022-02-24 17:07:01 -0300

    Segunda Entrega del Proyecto Final

--- funciones.js
+++ funciones.js
@@ -3,21 +3,41 @@ let lista1 = document.getElementById("Productos");
 
 const mostrar = (array1) => {
     for (const product of array1){
+      if ((itemsCarrito !== null) && (itemsCarrito.find(product1 => product1.id == product.id)) !== undefined) {
         lista.innerHTML += `
-      <div class="card" style="width: 18rem;">
-        <div class="imagen">
+        <div class="card" style="width: 18rem;">
+          <div class="imagen">
             <img src= ${product.imagen} class="card-img-top" alt="...">
-        </div>
-        <div class="card-body">
-          <h5 class="card-title">${product.nombre}</h5>
-        </div>
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item">Precio: $${product.precio}</li>
-          <li class="list-group-item">Plataforma: ${product.plataforma.join("/")}</li>
-          <li class="list-group-item">Categoria: ${product.categoria}</li>
-          <button id= "boton${product.id}" href="#" class="btn btn-primary">Añadir al Carrito</button>
-        </ul>
-    </div>`;
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">${product.nombre}</h5>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">Precio: $${product.precio}</li>
+            <li class="list-group-item">Plataforma: ${product.plataforma.join("/")}</li>
+            <li class="list-group-item">Categoria: ${product.categoria}</li>
+            <button disabled = true id= "boton${product.id}" href="#" class="btn btn-primary">En Carrito</button>
+          </ul>
+        </div>`;
+      }
+      else{
+        lista.innerHTML += `
+        <div class="card" style="width: 18rem;">
+          <div class="imagen">
+            <img src= ${product.imagen} class="card-img-top" alt="...">
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">${product.nombre}</h5>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">Precio: $${product.precio}</li>
+            <li class="list-group-item">Plataforma: ${product.plataforma.join("/")}</li>
+            <li class="list-group-item">Categoria: ${product.categoria}</li>
+            <button id= "boton${product.id}" href="#" class="btn btn-primary">Añadir al Carrito</button>
+          </ul>
+        </div>`;
+      }
+
     }
     lista1.append(lista);
     lista.id = "productStyle";
@@ -44,37 +64,32 @@ const AñadirCarrito = () => {
   let carro = document.getElementsByClassName("btn btn-primary");
   for (cart of carro) {
     cart.addEventListener("click", e => {
-    if ((itemsCarrito !== null) && (itemsCarrito.find(product => `boton${product.id}` == e.target.id)) !== undefined) {
-      e.target.innerText = "En Carrito";
-      e.target.disabled = true;
+    e.target.innerText = "Producto Añadido";
+    e.target.disabled = true;
+    const selected = productos.find(product => `boton${product.id}` == e.target.id);
+    nuevoArray2.push(selected);
+    if (itemsCarrito !== null) {
+      itemsCarrito.push(selected)
+      localStorage.setItem("carro", JSON.stringify(itemsCarrito));
+      itemsCarrito = JSON.parse(localStorage.getItem("carro"));
+      let contador = document.getElementById("circulo");
+      let cont = document.createElement("b");
+      cont.innerHTML = itemsCarrito.length;
+      $("#circulo").empty();
+      contador.appendChild(cont);
+      localStorage.setItem("cont", JSON.stringify(itemsCarrito.length));
+    } 
+    else{
+      localStorage.setItem("carro", JSON.stringify(nuevoArray2));
+      itemsCarrito = JSON.parse(localStorage.getItem("carro"));
+      let contador = document.getElementById("circulo");
+      let cont = document.createElement("b");
+      cont.innerHTML = nuevoArray2.length;
+      $("#circulo").empty();
+      contador.appendChild(cont);
+      localStorage.setItem("cont", JSON.stringify(nuevoArray2.length));
     }
-    else {
-      e.target.innerText = "Producto Añadido";
-      e.target.disabled = true;
-      const selected = productos.find(product => `boton${product.id}` == e.target.id);
-      nuevoArray2.push(selected);
-      if (itemsCarrito !== null) {
-        itemsCarrito.push(selected)
-        localStorage.setItem("carro", JSON.stringify(itemsCarrito));
-        itemsCarrito = JSON.parse(localStorage.getItem("carro"));
-        let contador = document.getElementById("circulo");
-        let cont = document.createElement("b");
-        cont.innerHTML = itemsCarrito.length;
-        $("#circulo").empty();
-        contador.appendChild(cont);
-        localStorage.setItem("cont", JSON.stringify(itemsCarrito.length));
-      } 
-      else{
-        localStorage.setItem("carro", JSON.stringify(nuevoArray2));
-        itemsCarrito = JSON.parse(localStorage.getItem("carro"));
-        let contador = document.getElementById("circulo");
-        let cont = document.createElement("b");
-        cont.innerHTML = nuevoArray2.length;
-        $("#circulo").empty();
-        contador.appendChild(cont);
-        localStorage.setItem("cont", JSON.stringify(nuevoArray2.length));
-      }
-    }
+    
     })
   }
 }
