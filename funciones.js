ff9311117485002f972568e056b7318cab1e00ae
Franco <francodlopez98@gmail.com>
2022-02-24 15:10:21 -0300

    Segunda Entrega del Proyecto Final

--- funciones.js
+++ funciones.js
@@ -78,3 +78,30 @@ const AñadirCarrito = () => {
     })
   }
 }
+
+
+let lista2 = document.createElement("div");
+let lista3 = document.getElementById("Productos1");
+
+
+const mostrar1 = (array1) => {
+    for (const product of array1){
+        lista2.innerHTML += `
+      <div class="card" style="width: 18rem;">
+        <div class="imagen">
+            <img src= ${product.imagen} class="card-img-top" alt="...">
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">${product.nombre}</h5>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Precio: $${product.precio}</li>
+          <li class="list-group-item">Plataforma: ${product.plataforma.join("/")}</li>
+          <li class="list-group-item">Categoria: ${product.categoria}</li>
+          <button id= "boton${product.id}" href="#" class="btn btn-primary">Quitar del Carrito</button>
+        </ul>
+    </div>`;
+    }
+    lista3.append(lista2);
+    lista2.id = "productStyle1";
+}
